1cc1b31b93d78bcc5725af303373c0a1116d5171
Jerred Shepherd <ShepherdJerred@gmail.com>
2020-12-04 21:58:08 -0800

    Implement Day 3 Part 1

--- Sources/Day3/main.swift
+++ Sources/Day3/main.swift
@@ -12,3 +12,117 @@ func arrayFromContentsOfFileWithName(fileName: String) -> [String]? {
         return nil;
     }
 }
+
+// Simplest way to throw an error/exception with a custom message in Swift 2?
+// https://stackoverflow.com/a/40629365
+extension String: Error {}
+
+func readMap() -> Map? {
+    guard let mapStringArray = arrayFromContentsOfFileWithName(fileName: "input") else {
+        return nil
+    }
+
+    do {
+        let entities: [[Entity]] = try mapStringArray.map {
+            try $0.map {
+                guard let entity = charToEntity(character: $0) else {
+                    throw "Unable to convert char to entity"
+                }
+                return entity
+            }
+        }
+
+        return Map(entities: entities)
+    } catch {
+        return nil
+    }
+}
+
+func charToEntity(character: Character) -> Entity? {
+    switch character {
+    case "#":
+        return Entity.TREE
+    case ".":
+        return Entity.NONE
+    default:
+        return nil
+    }
+}
+
+class Map {
+    let entities: [[Entity]]
+    let mapSize: MapSize
+
+    init(entities: [[Entity]]) {
+        self.entities = entities
+        mapSize = MapSize(
+            height: entities.count,
+            width: entities[0].count
+        )
+    }
+
+    func get(position: Position) -> Entity {
+        let adjustedX = position.x % mapSize.width
+        let adjustedY = position.y % mapSize.height
+        print(adjustedX, adjustedY)
+        return entities[adjustedY][adjustedX]
+    }
+
+    func isEntity(position: Position, entity: Entity) -> Bool {
+        get(position: position) == entity
+    }
+
+    func isTree(position: Position) -> Bool {
+        isEntity(position: position, entity: Entity.TREE)
+    }
+
+    func countCollision(position: Position) -> Int {
+        isTree(position: position) ? 1 : 0
+    }
+}
+
+struct MapSize {
+    let height: Int
+    let width: Int
+}
+
+struct MapTraversalStrategy {
+    let down: Int
+    let right: Int
+}
+
+struct Position {
+    let x: Int
+    let y: Int
+}
+
+func incrementPosition(position: Position, strategy: MapTraversalStrategy) -> Position {
+    Position(x: position.x + strategy.right, y: position.y + strategy.down)
+}
+
+func countCollisions(map: Map, strategy: MapTraversalStrategy, position: Position) -> Int {
+    if position.y > map.mapSize.height {
+        return 0
+    } else {
+        print(position)
+        return  map.countCollision(position: position) + countCollisions(map: map, strategy: strategy, position: incrementPosition(position: position, strategy: strategy))
+    }
+}
+
+enum Entity {
+    case NONE
+    case TREE
+}
+
+guard let map = readMap() else {
+    print("Unable to read map")
+    exit(1)
+}
+
+print(map.mapSize)
+
+let origin = Position(x: 0, y: 0)
+let strategy = MapTraversalStrategy(down: 1, right: 3)
+let collisions = countCollisions(map: map, strategy: strategy, position: origin)
+
+print(collisions)
